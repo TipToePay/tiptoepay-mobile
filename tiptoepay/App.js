ab67ebce799807e5171d1b4a3c16d9dae0af919c
PHIL H KIM <philhkim@gmail.com>
2018-06-26 01:06:39 -0400

    Registration page UI v.1 complete
    Signed-off-by: PHIL H KIM <philhkim@gmail.com>

--- tiptoepay/App.js
+++ tiptoepay/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import {
+  StackNavigator
+} from 'react-navigation';
+import * as Screens from './app/screens';
+import {AppLoading, Font} from 'expo';
+import {bootstrap} from './app/config/bootstrap';
+
+bootstrap();
+const TipToeApp = StackNavigator({
+  First: {
+    screen: Screens.SignUp
+  }
+});
 
 export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  componentWillMount() {
+    this._loadAssets();
+  }
+
+  _loadAssets = async() => {
+    await Font.loadAsync({
+      'fontawesome': require('./app/assets/fonts/fontawesome.ttf'),
+      'icomoon': require('./app/assets/fonts/icomoon.ttf'),
+      'Righteous-Regular': require('./app/assets/fonts/Righteous-Regular.ttf'),
+      'Roboto-Bold': require('./app/assets/fonts/Roboto-Bold.ttf'),
+      'Roboto-Medium': require('./app/assets/fonts/Roboto-Medium.ttf'),
+      'Roboto-Regular': require('./app/assets/fonts/Roboto-Regular.ttf'),
+      'Roboto-Light': require('./app/assets/fonts/Roboto-Light.ttf'),
+    });
+    this.setState({loaded: true});
+  };
+
   render() {
+    if (!this.state.loaded) {
+      return <AppLoading />;
+    }
+  
     return (
-      <View style={styles.container}>
-        <Text>TipToePay</Text>
-        <Text>First Commit : 6/20/2018</Text>
+      <View style={{flex: 1}}>
+        <TipToeApp/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
